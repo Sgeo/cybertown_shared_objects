e131f7f4518b92df4705ebb6704ef58a866f9eca
Sgeo <sgeoster@gmail.com>
2019-10-18 00:06:34 -0400

    List of SharedObjects from external file

--- 000/xite_so.js
+++ 000/xite_so.js
@@ -1,15 +1,27 @@
-X3D(function() {
-    let browser = X3D.getBrowser();
-    browser.addBrowserCallback(null, async function(eventType) {
-        console.log(eventType);
-        if(eventType === X3D.X3DConstants.INITIALIZED_EVENT) {
-            //let exp = browser.createVrmlFromString("Transform { translation 0 1.75 0 children[Inline { url \"/dbobjects/5000/5000exp.wrl\" }] }")[0];
-            let so = browser.currentScene.createProto("SharedObject")
-            so.translation = new X3D.SFVec3f(0, 1.75, 0);
-            let inline = browser.currentScene.createNode("Inline")
-            inline.url = new X3D.MFString("/dbobjects/5000/5000exp.wrl");
-            so.children[0] = inline;
-            browser.currentScene.addRootNode(so);
-        }
+(function() {
+    function addSO(browser, obj) {
+        let sharedObject = browser.currentScene.createProto("SharedObject");
+        sharedObject.name = obj.name;
+        sharedObject.id = obj.id;
+        sharedObject.translation = new X3D.SFVec3f(obj.position.x, obj.position.y, obj.position.z);
+        sharedObject.rotation = new X3D.SFRotation(obj.rotation.x, obj.rotation.y, obj.rotation.z, obj.rotation.angle);
+        let inline = browser.currentScene.createNode("Inline");
+        inline.url = new X3D.MFString(obj.url);
+        sharedObject.children[0] = inline;
+        browser.currentScene.addRootNode(sharedObject);
+    }
+    
+    X3D(function() {
+        let browser = X3D.getBrowser();
+        browser.addBrowserCallback(null, async function(eventType) {
+            console.log(eventType);
+            if(eventType === X3D.X3DConstants.INITIALIZED_EVENT) {
+                
+                let SOs = await fetch("SOs.json").then(resp => resp.json());
+                for(sharedObject of SOs) {
+                    addSO(browser, sharedObject);
+                }
+            }
+        });
     });
-});
+})();
