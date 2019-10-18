15739d81ee8c03d338950edcad130ea3cd22d557
Sgeo <sgeoster@gmail.com>
2019-10-17 23:51:18 -0400

    Piecemal creation of SharedObject node allows animations to start properly.

--- 000/xite_so.js
+++ 000/xite_so.js
@@ -3,8 +3,13 @@ X3D(function() {
     browser.addBrowserCallback(null, async function(eventType) {
         console.log(eventType);
         if(eventType === X3D.X3DConstants.INITIALIZED_EVENT) {
-            let exp = browser.createVrmlFromString("Transform { translation 0 1.75 0 children[Inline { url \"/dbobjects/5000/5000exp.wrl\" }] }")[0];
-            browser.currentScene.addRootNode(exp);
+            //let exp = browser.createVrmlFromString("Transform { translation 0 1.75 0 children[Inline { url \"/dbobjects/5000/5000exp.wrl\" }] }")[0];
+            let so = browser.currentScene.createProto("SharedObject")
+            so.translation = new X3D.SFVec3f(0, 1.75, 0);
+            let inline = browser.currentScene.createNode("Inline")
+            inline.url = new X3D.MFString("/dbobjects/5000/5000exp.wrl");
+            so.children[0] = inline;
+            browser.currentScene.addRootNode(so);
         }
     });
 });
