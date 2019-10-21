c5e3fe3781b6d055327b35dc7af3155922001a4c
Sgeo <sgeoster@gmail.com>
2019-10-20 22:16:40 -0400

    Wrap SharedObject PROTO in Group node Easy console access to Shared Objects.

--- 000/xite_so.js
+++ 000/xite_so.js
@@ -9,18 +9,21 @@
         inline.url = new X3D.MFString(obj.url);
         sharedObject.children[0] = inline;
         browser.currentScene.addRootNode(sharedObject);
+        return sharedObject
     }
     
     X3D(function() {
         console.log("Browser ready");
         let browser = X3D.getBrowser();
+        window.SOs = new Map();
         browser.addBrowserCallback({}, async function(eventType) {
             console.log(eventType);
             if(eventType === X3D.X3DConstants.INITIALIZED_EVENT) {
                 
                 let SOs = await fetch("SOs.json").then(resp => resp.json());
                 for(sharedObject of SOs) {
-                    addSO(browser, sharedObject);
+                    let sharedObjectInstance = addSO(browser, sharedObject);
+                    window.SOs.set(sharedObject.id, sharedObjectInstance);
                 }
             }
         });
