5077f91487d1aa7bfd60acfe9b43f151b42dbcc6
Sgeo <sgeoster@gmail.com>
2019-11-13 07:06:53 +0000

    Reorganize into public, initial server-side shared events impl

---
+++ public/xite_js/xite_av.js
@@ -0,0 +1,39 @@
+(function() {
+    
+    function loadInlineAsync(browser, url) {
+        browser.endUpdate();
+        let inline = browser.currentScene.createNode("Inline");
+        inline.url = new X3D.MFString(url);
+        let loadSensor = browser.currentScene.createNode("LoadSensor");
+        loadSensor.watchList[0] = inline;
+        let callbackKey = {};
+        let promise = new Promise(function(resolve, reject) {
+            loadSensor.addFieldCallback("isLoaded", callbackKey, function(value) {
+                loadSensor.removeFieldCallback("isLoaded", callbackKey);
+                if(value) {
+                    resolve(inline);
+                } else {
+                    reject(inline);
+                }
+            });
+        });
+        browser.beginUpdate();
+        return promise;
+    }
+    
+    X3D(function() {
+        let browser = X3D.getBrowser();
+        browser.addBrowserCallback({}, async function(eventType) {
+            console.log(eventType);
+            if(eventType === X3D.X3DConstants.INITIALIZED_EVENT) {
+                
+                let avInline = await loadInlineAsync(browser, "/avatars/default.wrl");
+                browser.currentScene.updateImportedNode(avInline, "Avatar", "DefaultAvatar");
+                window.avImport = browser.currentScene.getImportedNode("DefaultAvatar")
+                console.log(window.avImport);
+                browser.currentScene.addRootNode(avInline);
+                avImport.set_translation = new X3D.SFVec3f(0.0, 2.0, 0.0);
+            }
+        });
+    });
+})();
---
+++ public/xite_js/xite_so.js
@@ -0,0 +1,65 @@
+BxxEvents.addEventListener("SO:upload:position", e => console.log(e));
+BxxEvents.addEventListener("SO:upload:rotation", e => console.log(e));
+
+
+(function() {
+    function addSO(browser, obj) {
+        let sharedObject = browser.currentScene.createProto("SharedObject");
+        sharedObject.name = obj.name;
+        sharedObject.id = obj.id;
+        sharedObject.translation = new X3D.SFVec3f(obj.position.x, obj.position.y, obj.position.z);
+        sharedObject.rotation = new X3D.SFRotation(obj.rotation.x, obj.rotation.y, obj.rotation.z, obj.rotation.angle);
+        let inline = browser.currentScene.createNode("Inline");
+        inline.url = new X3D.MFString(obj.url);
+        sharedObject.children[0] = inline;
+        browser.currentScene.addRootNode(sharedObject);
+        
+        sharedObject.addFieldCallback("newPosition", {}, function(pos) {
+            BxxEvents.dispatchEvent(new CustomEvent("SO:upload:position", {
+                detail: {
+                    id: sharedObject.id,
+                    type: "position",
+                    value: {
+                        x: pos.x,
+                        y: pos.y,
+                        z: pos.z
+                    }
+                }
+            }));
+        });
+        
+        sharedObject.addFieldCallback("newRotation", {}, function(rot) {
+            BxxEvents.dispatchEvent(new CustomEvent("SO:upload:rotation", {
+                detail: {
+                    id: sharedObject.id,
+                    type: "rotation",
+                    value: {
+                        x: rot.x,
+                        y: rot.y,
+                        z: rot.z,
+                        angle: rot.angle
+                    }
+                }
+            }));
+        });
+        
+        return sharedObject
+    }
+    
+    X3D(function() {
+        console.log("Browser ready");
+        let browser = X3D.getBrowser();
+        window.SOs = new Map();
+        browser.addBrowserCallback({}, async function(eventType) {
+            console.log(eventType);
+            if(eventType === X3D.X3DConstants.INITIALIZED_EVENT) {
+                
+                let SOs = await fetch("SOs.json").then(resp => resp.json());
+                for(sharedObject of SOs) {
+                    let sharedObjectInstance = addSO(browser, sharedObject);
+                    window.SOs.set(sharedObject.id, sharedObjectInstance);
+                }
+            }
+        });
+    });
+})();
