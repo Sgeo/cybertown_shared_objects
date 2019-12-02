f485dbf059d2444a9b5cdcf6c99b99ffbedaa011
Sgeo <sgeoster@gmail.com>
2019-12-02 02:04:13 -0500

    * Retain avatar positions and transmit positions to clients on connect * Delay network connection until after scene is loaded

--- index.js
+++ index.js
@@ -15,8 +15,15 @@ app.get('/', (req, res) => {
   res.send('Hello Express app!')
 });
 
+let AVATARS = new Map();
+
 io.on('connection', function(socket){
   console.log('a user connected');
+  AVATARS.set(socket.id, {translation: { x: 0, y: 0, z: 0 }, rotation: { x: 0, y: 1, z: 0, angle: 0}});
+  for(let [id, av] of AVATARS) {
+    console.log("Preexisting:", [id, av]);
+    socket.emit("AV", {id: id, wrl: "/avatars/default.wrl", translation: av.translation, rotation: av.rotation});
+  }
   socket.on("SE", function(msg) {
     console.log(msg);
     io.emit("SE", msg);
@@ -26,6 +33,14 @@ io.on('connection', function(socket){
     msg.wrl = "/avatars/default.wrl";
     console.log(msg);
     socket.broadcast.emit("AV", msg);
+    if(AVATARS.get(socket.id)) {
+      if(msg.translation) {
+        AVATARS.get(socket.id).translation = msg.translation;
+      }
+      if(msg.rotation) {
+        AVATARS.get(socket.id).rotation = msg.rotation;
+      }
+    }
   });
 });
 
