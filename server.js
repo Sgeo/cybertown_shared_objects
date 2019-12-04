4f1efd96f31b7629bcb6f2ef162a3617cdb18f73
Sgeo <sgeoster@gmail.com>
2019-12-04 03:16:47 -0500

    Try and fail to fix loading edge case.

--- server.js
+++ server.js
@@ -19,6 +19,24 @@ let AVATARS = new Map();
 
 io.on('connection', async function(socket){
   console.log('a user connected');
+  AVATARS.set(socket, {translation: {x: 0, y: 0, z: 0}, rotation: {x: 0, y: 1, z: 0, angle: 0}});
+  socket.on("AV", function(msg) {
+    msg.id = socket.id;
+    msg.wrl = "/avatars/default.wrl";
+    console.log(msg);
+    console.log(AVATARS.get(socket).room);
+    if(AVATARS.get(socket) && AVATARS.get(socket).room) {
+      socket.to(AVATARS.get(socket).room).emit("AV", msg);
+    }
+    if(AVATARS.get(socket)) {
+      if(msg.translation) {
+        AVATARS.get(socket).translation = msg.translation;
+      }
+      if(msg.rotation) {
+        AVATARS.get(socket).rotation = msg.rotation;
+      }
+    }
+  });
   let initDetail;
   try {
     initDetail = await new Promise((resolve, reject) => { socket.on("JOIN", resolve); setTimeout(() => reject("Timed out waiting for JOIN"), 10000); });
@@ -30,31 +48,17 @@ io.on('connection', async function(socket){
     if(AVATARS.get(other_socket).room === initDetail.room) {
       console.log("Preexisting:", [other_socket.id, av]);
       socket.emit("AV", {id: other_socket.id, wrl: "/avatars/default.wrl", translation: av.translation, rotation: av.rotation});
-      other_socket.emit("AV", {id: socket.id, wrl: "/avatars/default.wrl", translation: initDetail.pos, rotation: initDetail.rot});
-      console.log("Telling other user that new avatar is", {id: socket.id, wrl: "/avatars/default.wrl", translation: initDetail.pos, rotation: initDetail.rot});
+      other_socket.emit("AV", {id: socket.id, wrl: "/avatars/default.wrl", translation: AVATARS.get(socket).translation, rotation: AVATARS.get(socket).rotation});
+      console.log("Telling other user that new avatar is", {id: socket.id, wrl: "/avatars/default.wrl", translation: AVATARS.get(socket).translation, rotation: AVATARS.get(socket).rotation});
     }
   }
   console.log(AVATARS);
-  AVATARS.set(socket, {translation: initDetail.pos, rotation: initDetail.rot, avatar: initDetail.avatar, room: initDetail.room});
+  AVATARS.get(socket).avatar = initDetail.avatar;
+  AVATARS.get(socket).room = initDetail.room;
   socket.join(initDetail.room);
   socket.on("SE", function(msg) {
     console.log(msg);
     io.to(AVATARS.get(socket).room).emit("SE", msg);
-  });
-  socket.on("AV", function(msg) {
-    msg.id = socket.id;
-    msg.wrl = "/avatars/default.wrl";
-    console.log(msg);
-    console.log(AVATARS.get(socket).room);
-    socket.to(AVATARS.get(socket).room).emit("AV", msg);
-    if(AVATARS.get(socket)) {
-      if(msg.translation) {
-        AVATARS.get(socket).translation = msg.translation;
-      }
-      if(msg.rotation) {
-        AVATARS.get(socket).rotation = msg.rotation;
-      }
-    }
   });
   socket.on("disconnect", function() {
     io.to(AVATARS.get(socket).room).emit("AV:del", socket.id);
